fa51c6c0d66461a05291888042399a10f50ef95c
DELL <suneertksuneer1999@gmail.com>
2023-08-04 15:27:24 -0700

    first commit

---
+++ src/ammu/Test.java
@@ -0,0 +1,14 @@
+package ammu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello");
+		System.out.print("pass");
+		System.out.print("false");
+		
+
+	}
+
+}
